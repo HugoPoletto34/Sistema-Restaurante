6cd78efb6c821c0f13603287b8137085d0b15f34
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-06-15 19:49:06 -0300

    novos testes

--- src/test/java/entites/ClienteTest.java
+++ src/test/java/entites/ClienteTest.java
@@ -72,12 +72,38 @@ public class ClienteTest {
 
         Cliente c = new Cliente("Pedro");
 
-        c.addPedido(new Pedido(new Data(), new Pizza()));
-        c.addPedido(new Pedido(new Data(), new Sanduiche()));
+        Pedido pedido1 = new Pedido(new Data(), new Pizza());
+        Pedido pedido2 = new Pedido(new Data(), new Sanduiche());
+
+        c.addPedido(pedido1);
+        c.addPedido(pedido2);
 
         ClienteDAO.add(c);
 
-        assertEquals(38.00, ClienteDAO.getReferenceById("Pedro").pedidoRecente().);
+        assertEquals(pedido2.getId(), ClienteDAO.getReferenceById("Pedro").pedidoRecente().getId());
+    
+    }
+
+    @Test
+    public void testAddItemPedido() throws IOException{
+
+        Cliente c = new Cliente("Pedro");
+
+        Pedido pedido1 = new Pedido(new Data(), new Pizza());
+        pedido1.addItem(new Pizza());
+
+        Pedido pedido2 = new Pedido(new Data(), new Sanduiche());
+
+        c.addPedido(pedido1);
+        c.addPedido(pedido2);
+
+        ClienteDAO.add(c);
+
+        assertEquals(3, ClienteDAO.getReferenceById("Pedro")
+        .getPedidos()
+        .stream()
+        .mapToInt(Pedido::getQuantItens)
+        .sum());
     
     }
 
