f12c0b227a4787a2135743bd841322c381748262
PedroBelo1550 <50001735+PedroBelo1550@users.noreply.github.com>
2022-06-15 19:39:16 -0300

    Implementação de novos testes

--- src/test/java/entites/ClienteTest.java
+++ src/test/java/entites/ClienteTest.java
@@ -1,6 +1,7 @@
 package entites;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.IOException;
 
@@ -8,6 +9,10 @@ import org.junit.jupiter.api.Test;
 
 import dao.ClienteDAO;
 import entities.Cliente;
+import entities.Data;
+import entities.Pedido;
+import entities.Pizza;
+import entities.Sanduiche;
 
 public class ClienteTest {
     
@@ -17,6 +22,62 @@ public class ClienteTest {
         Cliente c = new Cliente("Pedro");
         ClienteDAO.add(c);
         assertEquals("Pedro", ClienteDAO.getReferenceById("Pedro").getNome());
+    
+    }
+
+    @Test
+    public void testRelatorioPedidos() throws IOException{
+
+        Cliente c = new Cliente("Pedro");
+
+        c.addPedido(new Pedido(new Data(), new Pizza()));
+        c.addPedido(new Pedido(new Data(), new Sanduiche()));
+
+        ClienteDAO.add(c);
+
+        assertFalse(ClienteDAO.getReferenceById("Pedro").relatorioPedidos().isEmpty(), "O relatório do pedido está vazio!");
+    
+    }
+
+    @Test
+    public void testVlrMaiorPedido() throws IOException{
+
+        Cliente c = new Cliente("Pedro");
+
+        c.addPedido(new Pedido(new Data(), new Pizza()));
+        c.addPedido(new Pedido(new Data(), new Sanduiche()));
+
+        ClienteDAO.add(c);
+
+        assertEquals(25.00, ClienteDAO.getReferenceById("Pedro").pedidoMaiorValor().getValorPago());
+    
+    }
+
+    @Test
+    public void testVrlTotalPedido() throws IOException{
+
+        Cliente c = new Cliente("Pedro");
+
+        c.addPedido(new Pedido(new Data(), new Pizza()));
+        c.addPedido(new Pedido(new Data(), new Sanduiche()));
+
+        ClienteDAO.add(c);
+
+        assertEquals(38.00, ClienteDAO.getReferenceById("Pedro").totalEmPedidos());
+    
+    }
+
+    @Test
+    public void testPedidoRecente() throws IOException{
+
+        Cliente c = new Cliente("Pedro");
+
+        c.addPedido(new Pedido(new Data(), new Pizza()));
+        c.addPedido(new Pedido(new Data(), new Sanduiche()));
+
+        ClienteDAO.add(c);
+
+        assertEquals(38.00, ClienteDAO.getReferenceById("Pedro").pedidoRecente().);
     
     }
 
